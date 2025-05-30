30859473fbae24b7752ce8cd71876e1f6cf02a9c
user <126427971+theantigone@users.noreply.github.com>
2025-05-30 15:41:39 -0400

    Add Text to TextNodes functionality

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -114,3 +114,11 @@ def split_nodes_link(old_nodes):
         "[{text_content}]({url})" # Format string for link markdown
     )
 
+def text_to_textnodes(text):
+    node = TextNode(text, TextType.TEXT)
+    new_nodes = split_nodes_delimiter([node], '**', TextType.BOLD)
+    new_nodes = split_nodes_delimiter(new_nodes, '_', TextType.ITALIC)
+    new_nodes = split_nodes_delimiter(new_nodes, '`', TextType.CODE)
+    new_nodes = split_nodes_image(new_nodes)
+    new_nodes = split_nodes_link(new_nodes)
+    return new_nodes
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -294,6 +294,25 @@ class TestInlineMarkdown(unittest.TestCase):
             new_nodes
         )
 
+    def test_text_to_textnodes(self):
+        text = 'This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)'
+        new_text = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            new_text
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
